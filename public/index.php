67e703d472eca42848ce90fb926e2ce63c78b058
bek <bekhzod@inoviagroup.se>
2020-03-09 20:59:47 +0500

    Update configs

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@ echo 'Hello world!<br />';
 $host = 'mysql';
 $user = getenv('MYSQL_USER');
 $pass = getenv('MYSQL_PASSWORD');
+$base = getenv('MYSQL_DATABASE');
 
-$conn = mysqli_connect($host, $user, $pass);
+$conn = mysqli_connect($host, $user, $pass, $base);
 if (!$conn) {
     exit('Connection failed: ' . mysqli_connect_error() . PHP_EOL);
 }
